ca2c6589cfbc7b78990a7e795a294d39adfbb2da
nico <nico@zuku.www.zuku.com>
2023-11-03 20:11:13 +0300

    bg

--- app/(site)/contact/page.tsx
+++ app/(site)/contact/page.tsx
@@ -3,7 +3,7 @@ import Form from "@/components/Form";
 const contact = () => {
   return (
     <div>
-      <div className="w-full h-screen bg-opacity-10 bg-black relative before:content-[''] before:bg-[url(/contact.jpg)] before:bg-cover before:bg-center before:w-full before:h-full before:absolute before:top-0 before:left-0 before:-z-1">
+      <div className="w-full h-screen bg-opacity-10 bg-black relative before:content-[''] before:bg-[url(/contact.jpg)] before:bg-cover before:bg-no-repeat before:bg-center before:w-full before:h-full before:absolute before:top-0 before:left-0 before:-z-1">
         <div className="w-full h-full flex opacity-95 flex-col justify-center items-center">
           <h1 className="text-2xl text-white font-bold py-10">Get In Touch</h1>
           <p className="text-center justify-center items-center text-white font-bold px-20 text-xl">
