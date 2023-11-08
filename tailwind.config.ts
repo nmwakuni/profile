4ba6259711309d2b2c619f74234593d98c9f8fcb
nico <nico@zuku.www.zuku.com>
2023-11-08 14:44:01 +0300

    img

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,6 +27,7 @@ const config: Config = {
         "bg-img-1": "url('/about.jpg')",
         "bg-img-2": "url('/contact.jpg')",
         "bg-img-3": "url('/bg4.jpg')",
+        "bg-img-4": "url('/projects.jpg')",
         pattern: "url('/pattern.png')",
       },
     },
